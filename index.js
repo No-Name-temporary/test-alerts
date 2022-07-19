5988f593b7d1fd8b1dd111e8cb5b3f21589d3919
Tim Dronkers <tdronkers@gmail.com>
2022-07-19 17:22:43 -0600

    adding scaffold email client
    Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com>
    Co-authored-by: Miles Abbason <miles.abbason@gmail.com>
    Co-authored-by: Scott Graham <scttgrhm7+public@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const { formatBody } = require('./lib/formatBody');
+const { sendMail } = require('./lib/sendMail');
+
 exports.handler = async (event) => {
-  console.log(event);
+  const emailAddress = event.alerting.alerts[1].address;
+  // const { results } = event.results;
+  const formattedResults = formatBody(event.results);
+  sendMail(emailAddress, formattedResults)
+    .then((result) => console.log('sent mail... ', result))
+    .catch((err) => console.error(err.message));
 };
