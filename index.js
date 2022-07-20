0af732902bbd4cd161f34bed99220296e5baf29a
Tim Dronkers <tdronkers@gmail.com>
2022-07-20 08:35:56 -0600

    adding passing assertion to verify false result filter
    Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com>
    Co-authored-by: Miles Abbason <miles.abbason@gmail.com>
    Co-authored-by: Scott Graham <scttgrhm7+public@gmail.com>

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { sendMail } = require('./lib/sendMail');
 
 exports.handler = async (event) => {
   const emailAddress = event.alerting.alerts[1].address;
-  // const { results } = event.results;
-  const formattedResults = formatBody(event.results);
+  const { results } = event;
+  const formattedResults = formatBody(results);
   sendMail(emailAddress, formattedResults)
     .then((result) => console.log('sent mail... ', result))
     .catch((err) => console.error(err.message));
